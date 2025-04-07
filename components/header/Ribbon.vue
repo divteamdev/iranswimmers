5a738fc5ad3ab6bcdec94908f624ebf25252b919
erik <erikheydari@gmail.com>
2025-04-08 02:14:22 +0330

    add header ribbon component with promotional message and button

---
+++ components/header/Ribbon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div
+      class="header-ribbon w-full  h-14 bg-secondary text-secondary-foreground flex items-center justify-center gap-4">
+    <Button class="bg-background text-foreground">
+      پیشنهاد ویژه
+    </Button>
+    <span>
+              برای دیدن اخبار و اطلاع رسانی از تخفیف ها عضو کانال تلگرامی ایرانسوییمرز شوید
+      </span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
