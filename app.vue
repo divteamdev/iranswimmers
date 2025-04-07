f2731a16b747a211dec7c4a4c5b88290fd1ff0f9
erik <erikheydari@gmail.com>
2025-04-08 02:12:38 +0330

    refactor app.vue to remove API testing logic and integrate Toaster component for notifications

--- app.vue
+++ app.vue
@@ -1,20 +1,10 @@
 <script setup>
-import { useApiStore } from '~/stores/APIs';
-
-const apiStore = useApiStore();
-
-// Call this to test the API
-onMounted(() => {
-  apiStore.testShopListApi();
-});
+import { Toaster } from '@/components/ui/sonner'
 </script>
 
 <template>
-  <div>
-    <div v-if="apiStore.shopListLoading">Loading...</div>
-    <div v-else-if="apiStore.shopListError">Error: {{ apiStore.shopListError }}</div>
-    <div v-else-if="apiStore.shopListData">
-      <pre>{{ JSON.stringify(apiStore.shopListData, null, 2) }}</pre>
-    </div>
-  </div>
+  <NuxtLayout/>
+  <ClientOnly>
+    <Toaster />
+  </ClientOnly>
 </template>
