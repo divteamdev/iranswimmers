bffd59f63a171bcb6471a7f9bd896e42ba6d13dc
erik <erikheydari@gmail.com>
2025-04-07 09:17:49 +0330

    add API integration to app.vue for shop list data retrieval

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
+<script setup>
+import { useApiStore } from '~/stores/APIs';
+
+const apiStore = useApiStore();
+
+// Call this to test the API
+onMounted(() => {
+  apiStore.testShopListApi();
+});
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div v-if="apiStore.shopListLoading">Loading...</div>
+    <div v-else-if="apiStore.shopListError">Error: {{ apiStore.shopListError }}</div>
+    <div v-else-if="apiStore.shopListData">
+      <pre>{{ JSON.stringify(apiStore.shopListData, null, 2) }}</pre>
+    </div>
   </div>
 </template>
