9ef1a39296c2c1e403f740802b092d3f21b2941c
erik <erikheydari@gmail.com>
2025-04-08 02:13:23 +0330

    button shad-cn added

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -22,8 +22,11 @@ export const buttonVariants = cva(
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
         sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
+        lg: 'h-11 rounded-md px-6 has-[>svg]:px-4',
+        xl: 'h-12 rounded-md px-8 has-[>svg]:px-6 text-xl',
         icon: 'size-9',
+        'icon-lg': 'h-11 w-11 aspect-square text-lg',
+        'icon-xl': 'h-12 w-12 aspect-square text-xl',
       },
     },
     defaultVariants: {
