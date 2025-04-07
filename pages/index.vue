85591da7b356067674a59ae954d9b1f348a88aa7
erik <erikheydari@gmail.com>
2025-04-08 02:12:29 +0330

    base pages

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useApiStore } from '~/stores/APIs';
+import { Toaster } from '@/components/ui/sonner'
+
+const apiStore = useApiStore();
+
+</script>
+
+<template>
+  <h1 class="heading-1">
+    <span>صفحه نخست</span>
+  </h1>
+</template>
