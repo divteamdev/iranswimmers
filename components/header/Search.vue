038be871213d2b3ccb19aebb0a11799bca9294dd
erik <erikheydari@gmail.com>
2025-04-08 02:14:17 +0330

    add search component with input field and search button

---
+++ components/header/Search.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import {Input} from "~/components/ui/input";
+</script>
+
+<template>
+  <div class="flex">
+    <Input type="text" placeholder=" مدویو، عینک شنا، جمر،مایو اقتصادی و ... " class="grow h-11 rounded-l-none" />
+    <Button variant="default" size="icon-lg" class="rounded-r-none">
+      <Icon name="hugeicons:search-01" class="text-primary-foreground"/>
+    </Button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
