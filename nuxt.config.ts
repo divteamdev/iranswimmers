a8fa4f5a3e3d31d3cc6588fe513a1468eb584ae4
erik <erikheydari@gmail.com>
2025-04-07 09:18:23 +0330

    add Tailwind CSS integration and configure runtime settings in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,39 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import {process} from "std-env";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image']
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  runtimeConfig: {
+    public: {
+      baseURL: process.env.BASE_URL || 'https://api.irsm.div.team',
+      apiPath: process.env.API_PATH || '/api/',
+    }
+  },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    'shadcn-nuxt',
+    '@pinia/nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
 })
