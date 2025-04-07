98caa13bd6645684c42b2a24c5b84251b835c73e
erik <erikheydari@gmail.com>
2025-04-08 02:13:04 +0330

    add default layout with header component

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import TheHeader from "~/components/header/TheHeader.vue";
+</script>
+
+<template>
+  <TheHeader/>
+  <main class="container mx-auto">
+    <NuxtPage />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
