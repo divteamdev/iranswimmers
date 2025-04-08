c89ea0f7052dff45a72763eb2f6862d58d2ce6bd
erik <erikheydari@gmail.com>
2025-04-08 10:17:47 +0330

    refactor index.vue to integrate homeStore and enhance home page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,37 @@
 <script setup>
-import {useApiStore} from '~/stores/APIs';
-import {Toaster} from '@/components/ui/sonner'
+import {useHomeStore} from '~/stores/homeStore';
 
-const apiStore = useApiStore();
+const homeStore = useHomeStore();
 
+onMounted(async () => {
+  await homeStore.fetchHomeData();
+});
 </script>
 
 <template>
-  <HomeSlider class="irsm-container mb-20"/>
-  <HomeCategories class="irsm-container mb-20"/>
+  <div>
+    <section class="irsm-container mb-20">
+      <HomeSlider dir="rtl"/>
+    </section>
+    <section class="irsm-container mb-20">
+      <HomeCategories/>
+    </section>
+
+    <home-section-on-sale/>
+
+    <HomeSectionFullCarousel :carousel="homeStore.carousel1" banner="/images/home-banners/banner-5.webp"/>
+
+    <HomeSectionFullCarousel :carousel="homeStore.carousel5" banner="/images/home-banners/banner-6.webp"/>
+
+    <HomeSectionFullCarousel :carousel="homeStore.carousel2" banner="/images/home-banners/banner-9.webp"/>
+
+    <!-- Third Carousel -->
+    <section v-if="homeStore.carousel3.products?.length" class="irsm-container mb-20" dir="rtl">
+      <HomeSectionHeader :title="homeStore.carousel3.title" title-class="heading-5"
+                         :show-more-link="homeStore.carousel3.all"/>
+      <ProductCarousel :products="homeStore.carousel3.products"/>
+    </section>
+
+  </div>
+
 </template>
