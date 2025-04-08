76bc2700028fe3a50ebbb51920b7a1a23f10eb05
erik <erikheydari@gmail.com>
2025-04-08 05:04:56 +0330

    update default.vue to replace TheHeader with Header and adjust main container class

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 
-import TheHeader from "~/components/header/TheHeader.vue";
 </script>
 
 <template>
-  <TheHeader/>
-  <main class="container mx-auto">
-    <NuxtPage />
+  <Header/>
+  <main class="irsm-container">
+    <NuxtPage/>
   </main>
 </template>
 
