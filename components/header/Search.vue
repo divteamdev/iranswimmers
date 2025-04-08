1250c62b689953d6748cc9a91c75a18f0c7b1476
erik <erikheydari@gmail.com>
2025-04-08 05:05:25 +0330

    update Search.vue to enhance input field styling with rounded corners

--- components/header/Search.vue
+++ components/header/Search.vue
@@ -5,7 +5,7 @@ import {Input} from "~/components/ui/input";
 
 <template>
   <div class="flex">
-    <Input type="text" placeholder=" مدویو، عینک شنا، جمر،مایو اقتصادی و ... " class="grow h-12 rounded-l-none"/>
+    <Input type="text" placeholder=" مدویو، عینک شنا، جمر،مایو اقتصادی و ... " class="grow h-12 rounded-l-none rounded-r-xl"/>
     <Button variant="default" size="icon-lg" class="rounded-r-none">
       <Icon name="hugeicons:search-01" class="text-primary-foreground text-2xl"/>
     </Button>
