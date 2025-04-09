cd98e67443d84c43a5bc1d0765eb7f1f82b1dd70
erik <erikheydari@gmail.com>
2025-04-09 23:08:25 +0330

    add Newsletter component for email subscription

---
+++ components/footer/Newsletter.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h6 class="mb-4 heading-5 text-center md:text-start">
+      از تخفیفات با خبر شو
+    </h6>
+    <div class="flex gap-2">
+      <Input class="bg-muted text-muted-foreground text-sm" placeholder="ایمیل خود را وارد کنید"/>
+      <Button size="sm" class="w-fit">
+        <span class="text-sm">عضو شوید</span>
+      </Button>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
