7ae783f772a99547936df010c6b102fc750fff1c
erfan heydari <erikheydari@gmail.com>
2025-04-10 15:48:29 +0330

    add loop and showBannerSlot props to carousel component; refactor template structure for improved layout

--- components/product/carousel/index.vue
+++ components/product/carousel/index.vue
@@ -17,33 +17,46 @@ const props = defineProps({
   carouselItemClass: {
     type: String,
     default: 'pl-4 md:basis-1/2 lg:basis-1/5 xl:basis-1/6 min-h-[400px]'
+  },
+  loop: {
+    type: Boolean,
+    default: true
+  },
+  showBannerSlot: {
+    type: Boolean,
+    default: false
   }
 })
 </script>
 
 <template>
-  <Carousel
-      class="h-full"
-      :opts="{
-        align: 'start',
-        loop: true,
-        direction: 'rtl',
-        dragFree: true,
-      }"
-      :plugins="[autoplayPlugin]"
-  >
-    <CarouselContent class="-ml-4 h-full">
-      <CarouselItem v-for="(product, index) in products"
-                    :key="index"
-                    :class="carouselItemClass + ' max-w-[300px] min-w-[220px] w-auto'">
-        <ProductCard
-            :variant="productCardVariant"
-            :product="product" :index="index" class="h-full"/>
-      </CarouselItem>
-    </CarouselContent>
-  </Carousel>
+  <div class="relative flex">
+    <Carousel
+        class="h-full w-full"
+        :opts="{
+          align: 'start',
+          loop: loop,
+          direction: 'rtl',
+          dragFree: true,
+        }"
+        :plugins="[autoplayPlugin]"
+    >
+      <CarouselContent class="-ml-4 h-full">
+
+        <slot name="banner-placeholder"></slot>
+
+        <CarouselItem v-for="(product, index) in products"
+                      :key="index"
+                      :class="carouselItemClass + ' max-w-[300px] min-w-[220px] w-auto'">
+          <ProductCard
+              :variant="productCardVariant"
+              :product="product" :index="index" class="h-full"/>
+        </CarouselItem>
+      </CarouselContent>
+    </Carousel>
+  
+  </div>
 </template>
 
 <style scoped>
-
 </style>
