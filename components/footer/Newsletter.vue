8bc84fbfb8ffcba70951e9621b07508946f4624c
erfan heydari <erikheydari@gmail.com>
2025-04-11 05:45:31 +0330

    Update Newsletter component typography for improved consistency and readability

--- components/footer/Newsletter.vue
+++ components/footer/Newsletter.vue
@@ -4,13 +4,13 @@
 
 <template>
   <div>
-    <h6 class="mb-4 heading-5 text-center md:text-start">
+    <h6 class="mb-4 heading-6 text-center md:text-start">
       از تخفیفات با خبر شو
     </h6>
     <div class="flex gap-2">
-      <Input class="bg-muted text-muted-foreground text-sm" placeholder="ایمیل خود را وارد کنید"/>
+      <Input class="bg-muted text-muted-foreground body-4" placeholder="ایمیل خود را وارد کنید"/>
       <Button size="sm" class="w-fit">
-        <span class="text-sm">عضو شوید</span>
+        <span class="body-4">عضو شوید</span>
       </Button>
     </div>
   </div>
