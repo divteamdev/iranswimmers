00feddd1f7e6ce3fe761a1d69e9153db616aa01e
erfan heydari <erikheydari@gmail.com>
2025-04-11 05:45:17 +0330

    Refactor About component typography for improved readability and consistency

--- components/footer/About.vue
+++ components/footer/About.vue
@@ -9,17 +9,17 @@ const toggleReadMore = () => {
 
 <template>
   <div>
-    <h6 class="heading-5 text-center mb-4 md:text-start ">
+    <h6 class="heading-6 text-center mb-4 md:text-start ">
       درباره ما
     </h6>
     <div class="relative">
-      <p class="body-2 text-start" :class="{ 'line-clamp-3': !isExpanded }">
+      <p class="body-3 text-start" :class="{ 'line-clamp-3': !isExpanded }">
         ایران سوییمرز، بزرگترین فروشگاه تخصصی محصولات شنا در ایران. ایران سوییمرز با 13 سال سابقه فروش بهترین برندهای شنای دنیا، از جمله ارنا، اسپیدو، زاگز، مدویو، نابایجی، توربو، سوانس، فینیس، میکاسا . فروشگاه لوازم شنا ایران سوییمرز توسط شناگران اسبق تیم ملی ایران اداره می‌شود.
       </p>
       <Button
           variant="link"
           size="sm"
-          class="mt-1 text-primary px-0"
+          class="mt-1 text-primary px-0 body-4"
           @click="toggleReadMore"
       >
         {{ isExpanded ? 'نمایش کمتر' : 'ادامه مطلب' }}
