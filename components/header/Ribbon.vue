d93dfb9b9934e0f5e849319b2b05092a2e36686e
erfan heydari <erikheydari@gmail.com>
2025-04-12 14:59:22 +0330

    Refactor Ribbon component for improved responsiveness and content update. Adjust height and modify text for mobile visibility, ensuring a better user experience across devices.

--- components/header/Ribbon.vue
+++ components/header/Ribbon.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <div
-      class="header-ribbon w-full  h-14 bg-secondary text-secondary-foreground flex items-center justify-center gap-4">
+  <div class="w-full h-12 bg-secondary text-secondary-foreground flex items-center justify-center gap-4">
     <Button size="sm" class="bg-background text-foreground hover:bg-background/95">
       پیشنهاد ویژه
     </Button>
-    <span class="caption-1">
-              برای دیدن اخبار و اطلاع رسانی از تخفیف ها عضو کانال تلگرامی ایرانسوییمرز شوید
-      </span>
+    <span class="body-4 lg:hidden">اینستاگرام ما را دنبال کنید</span>
+    <span class="caption-1 hidden lg:block">
+      برای دیدن اخبار و اطلاع رسانی از تخفیف ها عضو کانال تلگرامی ایرانسوییمرز شوید
+    </span>
   </div>
 </template>
 
