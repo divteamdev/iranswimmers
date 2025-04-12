b8382f31429cbdae09092de63239f4851f435dfe
erfan heydari <erikheydari@gmail.com>
2025-04-12 14:58:55 +0330

    Add Logo component to header for branding with responsive layout and logo image integration.

---
+++ components/header/Logo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-w-[10%] flex justify-center md:justify-start absolute md:relative left-1/2 md:left-0 transform -translate-x-1/2 md:translate-x-0">
+    <NuxtLink to="/" class="flex items-center gap-2">
+      <img src="/images/logo.png" alt="Logo" class="w-12 h-12 md:w-16 md:h-16"/>
+      <span class="text-primary font-bold font-fa-num hidden lg:inline">
+        ایران سوییمرز
+      </span>
+    </NuxtLink>
+  </div>
+</template> 
