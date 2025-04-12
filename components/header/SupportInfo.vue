85ff18031aa3a978d16998c3e2bd4cd2c164d9aa
erfan heydari <erikheydari@gmail.com>
2025-04-12 14:59:33 +0330

    Add SupportInfo component to header for customer support contact. Include button with icon and phone number for enhanced user assistance.

---
+++ components/header/SupportInfo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <Button variant="ghost" size="lg">
+    <Icon name="hugeicons:customer-support" class="text-secondary text-2xl"/>
+    <span dir="ltr" class="text-lg font-normal">0921 234 4566</span>
+  </Button>
+</template> 
