261ddad7cb4c17ae39919292d125d20d18c05364
erfan heydari <erikheydari@gmail.com>
2025-04-12 14:59:42 +0330

    Enhance Card component with hover effect and transition for improved user interaction. Update class bindings to include hover background color and smooth transition duration.

--- components/product/card/index.vue
+++ components/product/card/index.vue
@@ -53,7 +53,7 @@ const cardVariants = computed(() => {
 <template>
   <Card
       dir="rtl"
-      class="justify-between min-h-[310px] md:min-h-[280px] lg:min-h-[340px] 2xl:min-w-[210px]"
+      class="justify-between min-h-[310px] md:min-h-[280px] lg:min-h-[340px] 2xl:min-w-[210px] hover:bg-card-hover transition-colors duration-300"
       :class="cardVariants.card"
       :to="`/product/${product.slug}`">
     <CardHeader>
