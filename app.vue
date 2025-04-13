6f0fe5001db1f8a89e13851e72bb2c6262216390
erfan heydari <erikheydari@gmail.com>
2025-04-13 17:20:23 +0330

    Add device detection and Toaster component to app.vue. Integrate mobile detection logic and ensure Toaster is rendered conditionally for improved user experience.

--- app.vue
+++ app.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { Toaster } from '@/components/ui/sonner'
+
+import { useDeviceDetection } from '~/composables/useDeviceDetection'
+
+const { isMobile } = useDeviceDetection()
+const { isActive } = useSearch()
 </script>
 
 <template>
   <NuxtLayout/>
+
   <ClientOnly>
     <Toaster />
   </ClientOnly>
