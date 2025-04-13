fa1bc4698f807ecba6d967dae43cb29b9b605c2b
erfan heydari <erikheydari@gmail.com>
2025-04-13 08:36:03 +0330

    Add MenuItem component for mobile menu. Implement TypeScript props for text, navigation link, and optional icons, enhancing mobile navigation and user interaction.

---
+++ components/menu/mobile/MenuItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+  to: string
+  prependIcon?: string
+  prependIconColor?: string
+  appendIcon?: string
+}>()
+</script>
+
+<template>
+  <NuxtLink :to="to" 
+            class="w-full max-w-[500px] flex px-4 py-4 items-center h-fit justify-between border-b border-b-border/30 last:border-b-0">
+    <i v-if="prependIcon" :class="[prependIcon, prependIconColor || 'text-primary', 'ml-2 text-lg']" />
+    <span class="w-full">{{ text }}</span>
+    <i v-if="appendIcon" :class="[appendIcon, 'text-xs text-primary']" />
+  </NuxtLink>
+</template> 
