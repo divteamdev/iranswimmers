b40f835301f368f3ab77502dafcf8f8b285097d0
erfan heydari <erikheydari@gmail.com>
2025-04-14 10:47:24 +0330

    Add Sheet component implementation. Create a new Sheet.vue file that utilizes DialogRoot from reka-ui, allowing for flexible slot usage and props forwarding for enhanced component integration.

---
+++ components/ui/sheet/Sheet.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { DialogRoot, type DialogRootEmits, type DialogRootProps, useForwardPropsEmits } from 'reka-ui'
+
+const props = defineProps<DialogRootProps>()
+//extend props
+const emits = defineEmits<DialogRootEmits>()
+
+const forwarded = useForwardPropsEmits(props, emits)
+</script>
+
+<template>
+  <DialogRoot
+    data-slot="sheet"
+    v-bind="forwarded"
+  >
+    <slot />
+  </DialogRoot>
+</template>
