e996deb046c6d92ee465e81c5825fc4af0f9c609
erfan heydari <erikheydari@gmail.com>
2025-04-14 10:48:04 +0330

    Add imports for UI components in default layout. Integrate Sheet, Button, Input, and Label components to enhance layout functionality and maintain consistency across the application.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-
+import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger, SheetClose } from "~/components/ui/sheet"
+import { Button } from "~/components/ui/button"
+import { Input } from "~/components/ui/input"
+import { Label } from "~/components/ui/label"
 </script>
 
 <template>
   <Header/>
+
   <main class="py-8">
     <NuxtPage/>
   </main>
