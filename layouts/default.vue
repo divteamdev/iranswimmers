20828277f22e18eeb4c9b89fe5e7b3f9312d74a4
erfan heydari <erikheydari@gmail.com>
2025-04-14 22:14:32 +0330

    Refactor default layout to improve component structure. Replace direct imports of UI components with Header, Footer, and BottomNavigation components for better organization. Clean up template syntax and ensure consistent formatting.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger, SheetClose } from "~/components/ui/sheet"
-import { Button } from "~/components/ui/button"
-import { Input } from "~/components/ui/input"
-import { Label } from "~/components/ui/label"
+import Header from '~/components/header/index.vue';
+import Footer from '~/components/footer/index.vue';
+import BottomNavigation from '~/components/bottomNavigation/index.vue';
 </script>
 
 <template>
-  <Header/>
+  <Header />
 
   <main class="py-8">
-    <NuxtPage/>
+    <NuxtPage />
   </main>
-  <Footer class="pb-16 lg:pb-0"/>
-  <BottomNavigation/>
+  <Footer class="pb-16 lg:pb-0" />
+  <BottomNavigation />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
