698ad6c4648fc8beb8876774edd2eac6c373def8
erik <erikheydari@gmail.com>
2025-04-19 00:29:23 +0330

    feat(breadcrumbs): Add Breadcrumb component with dynamic formatting and new styles

---
+++ components/ui/breadcrumb/BreadcrumbSeparator.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <li
+    data-slot="breadcrumb-separator"
+    role="presentation"
+    aria-hidden="true"
+    :class="cn( 'flex items-center justify-center' ,props.class)"
+  >
+    <slot>
+      <Icon name="hugeicons:arrow-left-01" class="text-base"/>
+    </slot>
+  </li>
+</template>
