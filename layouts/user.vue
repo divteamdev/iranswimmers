2ae590141ded789421c6dff8f9957c0cb7030bb1
erik <erikheydari@gmail.com>
2025-04-20 22:07:04 +0330

    feat(user): Create user component with basic layout and structure

---
+++ layouts/user.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="min-h-screen flex flex-col items-center justify-center">
+    <NuxtPage/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
