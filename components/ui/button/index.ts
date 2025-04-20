335c9d8dfb5309688ea9642edfc38f522bfba42b
erik <erikheydari@gmail.com>
2025-04-20 22:06:29 +0330

    fix(ui): Correct button outline and size styles for consistency

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -12,7 +12,7 @@ export const buttonVariants = cva(
         destructive:
           'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
-          'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+          'border bg-background border border-border hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
         secondary:
           'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         accent:
@@ -24,7 +24,7 @@ export const buttonVariants = cva(
       size: {
         default: 'h-9 px-4 py-2 button-text-1',
         sm: 'h-8 rounded-md gap-1.5 px-3 button-text-2',
-        lg: 'h-12 rounded-xl button-text-1 px-4 [&_.iconify]:text-xl has-[>.iconify]:px-3.5',
+        lg: 'h-12 rounded-lg button-text-1 px-4 [&_.iconify]:text-xl has-[>.iconify]:px-3.5',
         xl: 'h-12 rounded-md px-8 text-xl',
         'icon-sm': '[&_.iconify]:text-lg rounded text-base aspect-square h-6 w-6',
         icon: 'size-9 rounded-md',
